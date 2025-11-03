84ab0295d2730134ac7884834b6332f7726853cc
Noddy03 <139820348+Noddy03@users.noreply.github.com>
2025-11-03 22:55:18 +0100

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,18 @@
 #include "LldAdapter.h"
 #include "GoldAdapter.h"
+#include <iostream>
 
 int main() {
+    std::cout << "Starting Heimdall Adapters test...\n";
+
+    // Create GoldAdapter instance
     heimdall::gold::GoldAdapter gold;
     gold.doSomething();
 
+    // Create LldAdapter instance
     heimdall::lld::LldAdapter lld;
     lld.doSomething();
 
+    std::cout << "Heimdall Adapters test finished.\n";
     return 0;
 }
