a1404b4f4d7cabeed79bba168f2a3abb239267d8
Noddy03 <139820348+Noddy03@users.noreply.github.com>
2025-11-03 17:12:57 +0100

    Create GoldAdapter.cpp

---
+++ src/gold/GoldAdapter.cpp
@@ -0,0 +1,16 @@
+#include "GoldAdapter.h"
+#include <iostream>
+
+namespace heimdall {
+namespace gold {
+
+GoldAdapter::GoldAdapter() {
+    std::cout << "GoldAdapter initialized\n";
+}
+
+void GoldAdapter::doSomething() {
+    std::cout << "GoldAdapter doing something\n";
+}
+
+} // namespace gold
+} // namespace heimdall
