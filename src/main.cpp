125d5a3156ff72be8ca49b95ce5b4b1c963d7c96
Noddy03 <139820348+Noddy03@users.noreply.github.com>
2025-11-03 22:56:31 +0100

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,14 @@
-#include "LldAdapter.h"
-#include "GoldAdapter.h"
+#include "lld/LLDAdapter.h"
+#include "gold/GoldAdapter.h"
 #include <iostream>
 
 int main() {
     std::cout << "Starting Heimdall Adapters test...\n";
 
-    // Create GoldAdapter instance
     heimdall::gold::GoldAdapter gold;
     gold.doSomething();
 
-    // Create LldAdapter instance
-    heimdall::lld::LldAdapter lld;
+    heimdall::lld::LLDAdapter lld;
     lld.doSomething();
 
     std::cout << "Heimdall Adapters test finished.\n";
